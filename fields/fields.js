9470020ddfb58f531edf8c33cfcda9cf0262433e
Roman Kitsela <roman.kitsela@gmail.com>
2023-04-25 23:03:54 +0100

    Create fields.js

---
+++ fields/fields.js
@@ -0,0 +1,101 @@
+// Vector Field 1
+
+function vf_1(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #1";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+// Vector Field 2
+
+function vf_2(x,y){
+
+    console.log('vf_2')
+    field_name_elt.innerHTML = "Vector Field #2";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(x,y)
+}
+
+// Vector Field 3
+
+function vf_3(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #3";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+// Vector Field 4
+
+function vf_4(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #4";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+// Vector Field 5
+
+function vf_5(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #5";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+
+// Vector Field 6
+
+function vf_6(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #6";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+// Vector Field 7
+
+function vf_7(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #7";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+// Vector Field 8
+
+function vf_8(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #8";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+// Vector Field 9
+
+function vf_9(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #9";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
+
+// Vector Field 10
+
+function vf_10(x,y){
+
+    field_name_elt.innerHTML = "Vector Field #10";
+    field_name_elt.style.color = '#65C6FF';
+
+    return new p5.Vector(y,x)
+}
