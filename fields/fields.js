0975b9c552b6c103774f647dfeb3268422a012f6
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-05 15:44:03 +0100

    Update fields.js
    Added new vector fields, improved naming

--- fields/fields.js
+++ fields/fields.js
@@ -1,101 +1,178 @@
-// Vector Field 1
+// Gradient Field
 
-function vf_1(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #1";
+function vf_gradient(x, y){
+    field_name_elt.innerHTML = "Gradient Field";
     field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(x, y);
+}
 
+// Hyperbolic Field
+
+function vf_hyperbolic(x,y){
+    field_name_elt.innerHTML = "Hyperbolic Field";
+    field_name_elt.style.color = '#65C6FF';
     return new p5.Vector(y,x)
 }
 
-// Vector Field 2
+// Circular Field
 
-function vf_2(x,y){
-
-    console.log('vf_2')
-    field_name_elt.innerHTML = "Vector Field #2";
+function vf_circular(x, y){
+    field_name_elt.innerHTML = "Circular Field";
     field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(x,y)
-}
-
-// Vector Field 3
-
-function vf_3(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #3";
-    field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(y,x)
-}
-
-// Vector Field 4
-
-function vf_4(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #4";
-    field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(y,x)
-}
-
-// Vector Field 5
-
-function vf_5(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #5";
-    field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(y,x)
+    return new p5.Vector(-y, x);
 }
 
 
-// Vector Field 6
+// Uniform Field
 
-function vf_6(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #6";
+function vf_uniform(x, y){
+    field_name_elt.innerHTML = "Uniform Field";
     field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(y,x)
+    return new p5.Vector(1, 1);
 }
 
 // Vector Field 7
 
-function vf_7(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #7";
+function vf_skew_hyperbolic(x, y){
+    field_name_elt.innerHTML = "Skew Hyperbolic Field";
     field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(y,x)
+    return new p5.Vector(y - x, y + x);
 }
 
-// Vector Field 8
 
-function vf_8(x,y){
+// Sine Cosine Field
 
-    field_name_elt.innerHTML = "Vector Field #8";
+function vf_sine_cosine(x, y){
+    field_name_elt.innerHTML = "Sine Cosine Field";
     field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(y,x)
+    return new p5.Vector(Math.sin(2*TWO_PI*x/width), Math.cos(3*TWO_PI*y/height));
 }
 
-// Vector Field 9
+// Rotating Sine Field
 
-function vf_9(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #9";
+function vf_rotating_sine(x, y){
+    field_name_elt.innerHTML = "Rotating Sine Field";
     field_name_elt.style.color = '#65C6FF';
-
-    return new p5.Vector(y,x)
+    return new p5.Vector(Math.sin(12*y/height), Math.cos(16*x/width));
 }
 
-// Vector Field 10
+// Parabolic Spiral Field
 
-function vf_10(x,y){
-
-    field_name_elt.innerHTML = "Vector Field #10";
+function vf_parabolic_spiral(x, y){
+    field_name_elt.innerHTML = "Parabolic Spiral Field";
     field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(y*y - x*x, 2*x*y);
+}
 
-    return new p5.Vector(y,x)
+// Quadratic Field A
+
+function vf_quadratic_A(x, y){
+    field_name_elt.innerHTML = "Quadratic Field A";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(x**2, y**2);
+}
+
+// Quadratic Field B
+
+function vf_quadratic_B(x, y){
+    field_name_elt.innerHTML = "Quadratic Field B";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(y**2,x**2);
+}
+
+// Cubic Field A
+
+function vf_cubic_A(x, y){
+    field_name_elt.innerHTML = "Cubic Field A";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(x**3, y**3);
+}
+
+// Cubic Field B
+
+function vf_cubic_B(x, y){
+    field_name_elt.innerHTML = "Cubic Field B";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(y**3,x**3);
+}
+
+// Improper Nodal Source
+
+function vf_improper_nodal_source(x, y){
+    field_name_elt.innerHTML = "Improper Nodal Source";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(15*x-1*y,16*x+7*y);
+}
+
+// Improper Nodal Sink
+
+function vf_improper_nodal_sink(x, y){
+    field_name_elt.innerHTML = "Improper Nodal Sink";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(-2*x+y,-4*x-6*y);
+}
+
+// Spiral Source
+
+function vf_spiral_source(x, y){
+    field_name_elt.innerHTML = "Spiral Source";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(-1*x-4*y,2*x+3*y);
+}
+
+// Stable Center
+
+function vf_stable_center(x, y){
+    field_name_elt.innerHTML = "Stable Center";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(x-2*y,x-y);
+}
+
+// Spiral Sink
+
+function vf_spiral_sink(x, y){
+    field_name_elt.innerHTML = "Spiral Sink";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(-1*x-4*y,2*x-3*y);
+}
+
+
+// THESE NEED REWORKING
+
+// Whirlpool Field -- Needs reworking
+
+function vf_whirlpool(x, y){
+    field_name_elt.innerHTML = "Whirlpool Field";
+    field_name_elt.style.color = '#65C6FF';
+    let denominator = x*x + y*y + 0.001;
+    return new p5.Vector(-y, x).mult(1/denominator);
+}
+
+// Tangent Field
+
+function vf_tangent(x, y){
+    field_name_elt.innerHTML = "Tangent Field";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(Math.tan(TWO_PI*x/width), Math.tan(2*TWO_PI*y/width));
+}
+
+
+// Exponential Field
+
+function vf_exp(x, y){
+    field_name_elt.innerHTML = "Exponential Field";
+    field_name_elt.style.color = '#65C6FF';
+    return new p5.Vector(Math.exp(x - y)/width, Math.exp(y - x)/height);
+}
+
+
+// Logarithmic Spiral Field
+
+function vf_log_spiral(x, y){
+    field_name_elt.innerHTML = "Logarithmic Spiral Field";
+    field_name_elt.style.color = '#65C6FF';
+    const rho = Math.sqrt(x * x + y * y);
+    const theta = Math.atan2(y, x);
+    const multiplier = rho === 0 ? 0 : Math.log(rho) / (rho * rho);
+    return new p5.Vector(-y * multiplier, x * multiplier);
 }
