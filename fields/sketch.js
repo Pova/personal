8eae80e807026c2e6d4c9500deca164110255ac7
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-05 15:42:51 +0100

    Update sketch.js
    Added new vector fields, updated dict of vector fields, refactored code into define_flowfield function

--- fields/sketch.js
+++ fields/sketch.js
@@ -11,21 +11,26 @@ let path_thickness;
 let toggle_flow = false;
 let boxes = false; //this will control debug mode
 
-let vec_index = 1;
+let current_field = "Gradient Field";
 const vec_dict = {
-  1:vf_1,
-  2:vf_2,
-  3:vf_3,
-  4:vf_4,
-  5:vf_5,
-  6:vf_6,
-  7:vf_7,
-  8:vf_8,
-  9:vf_9,
-  10:vf_10
+  "Gradient Field":vf_gradient,
+  "Hyperbolic Field":vf_hyperbolic,
+  "Circular Field":vf_circular,
+  "Uniform Field":vf_uniform,
+  "Skew Hyperbolic Field":vf_skew_hyperbolic,
+  "Sine Cosine Field":vf_sine_cosine,
+  "Rotating Sine Field":vf_rotating_sine,
+  "Parabolic Spiral Field":vf_parabolic_spiral,
+  "Quadratic Field A":vf_quadratic_A,
+  "Quadratic Field B":vf_quadratic_B,
+  "Cubic Field A":vf_cubic_A,
+  "Cubic Field B":vf_cubic_B,
+  "Improper Nodal Source":vf_improper_nodal_source,
+  "Improper Nodal Sink":vf_improper_nodal_sink,
+  "Spiral Source":vf_spiral_source,
+  "Stable Center":vf_stable_center,
+  "Spiral Sink":vf_spiral_sink
 };
-
-
 
 function windowResized() {
   setup();
@@ -61,26 +66,17 @@ function setup() {
   field_name_elt = document.getElementById('field_name');
   field_name_elt.style.color = '#65C6FF';
 
-  let yoff = -canvasHeight/2; 
-  for (let y = 0; y < rows; y++) {
-    let xoff = -canvasWidth/2;
-    for (let x = 0; x < cols; x++) {
-      let index = x + y * cols;
-      //v = new p5.Vector(yoff,xoff);
-      v = vec_dict[vec_index](xoff, yoff);
-      flowfield[index] = v;
-      xoff += scl;
-    }
-    yoff += scl;
-  }
+  define_flowfield()
 }
 
 function draw() {
+
+  console.log(mouseX,mouseY);
+
   background(0,0,0,10);
   smooth();
 
   translate(canvasWidth/2, canvasHeight/2);
-
 
   for (let i = 0; i < particles.length; i++) {
     particles[i].edges();
@@ -90,7 +86,6 @@ function draw() {
     particles[i].aging();
     particles[i].checkdeath();
   }
-
 }
 
 function clearBG() {
@@ -112,8 +107,33 @@ function show_ff() {
   }
 }
 
-function vecChoice(n){
+function vecChoice(field_name){
   resetParticles();
-  console.log(n);
-  vec_index = n;
+  current_field = field_name;
+  define_flowfield()
+
+  // Visually distinguish the selected button
+  let buttons = document.getElementById('vectorButtons').getElementsByTagName('button');
+  for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i].innerHTML === field_name) {
+          buttons[i].classList.add('btn-selected'); // Add 'btn-selected' class to selected button
+      } else {
+          buttons[i].classList.remove('btn-selected'); // Remove 'btn-selected' class from other buttons
+      }
+  }
+
+}
+
+function define_flowfield(){
+  let yoff = -canvasHeight/2; 
+  for (let y = 0; y < rows; y++) {
+    let xoff = -canvasWidth/2;
+    for (let x = 0; x < cols; x++) {
+      let index = x + y * cols;
+      v = vec_dict[current_field](xoff, yoff);
+      flowfield[index] = v;
+      xoff += scl;
+    }
+    yoff += scl;
+  }
 }
