85d76e29e794472890608dda735d79f9269856e3
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-06 15:35:28 +0100

    Refactor code

--- fields/sketch.js
+++ fields/sketch.js
@@ -1,17 +1,27 @@
-let scl = 3; //size of each grid element
-let cols, rows; //rows and columns of grid
+// Constants and configurations
+const scl = 3; //size of each grid element
+const z_inc = 0.00007;
 
-let zoff = 0; //initialized zoff value, increments with z_inc
-let z_inc = 0.00007;
-
-let fr;
-let particles = [];
-const flowfield = new Array();
+// Page elements
 let path_thickness;
+let fr;
 let toggle_flow = false;
 let boxes = false; //this will control debug mode
+let show_axes = false; // show axes for debug
 
+// Grid configuration
+let cols, rows;
+let zoff = 0; //initialized zoff value, increments with z_inc
+
+// Particles and fields
+let particles = [];
+const flowfield = new Array();
 let current_field = "Gradient Field";
+let canvasHeight, canvasWidth;
+let hue_change_val;
+
+// Store vector formulas and corresponding functions in dictionaries
+// Referenced when updating the displayed formula and defining the flow field
 const vec_dict = {
   "Gradient Field":vf_gradient,
   "Hyperbolic Field":vf_hyperbolic,
@@ -32,19 +42,28 @@ const vec_dict = {
   "Spiral Sink":vf_spiral_sink
 };
 
-function windowResized() {
-  setup();
-}
+const vecFormulas = {
+  "Gradient Field":" \\mathbf{F}(x,y) = x\\mathbf{i} + y\\mathbf{j}",
+  "Hyperbolic Field":"\\mathbf{F}(x,y) = y\\mathbf{i} + x\\mathbf{j}",
+  "Circular Field":"\\mathbf{F}(x,y) = -y\\mathbf{i} + x\\mathbf{j}",
+  "Uniform Field":"\\mathbf{F}(x,y) = 1 \\mathbf{i} - 1 \\mathbf{j}",
+  "Skew Hyperbolic Field":"\\mathbf{F}(x,y) = (y-x)\\mathbf{i} + (y+x)\\mathbf{j}",
+  "Sine Cosine Field":"\\mathbf{F}(x,y) = sin(\\alpha x)\\mathbf{i} + cos(\\beta y)\\mathbf{j}",
+  "Rotating Sine Field":"\\mathbf{F}(x,y) = sin(\\alpha y)\\mathbf{i} + cos(\\beta x)\\mathbf{j}",
+  "Parabolic Spiral Field":"\\mathbf{F}(x,y) = (y^2-x^2)\\mathbf{i} + 2xy\\mathbf{j}",
+  "Quadratic Field A":"\\mathbf{F}(x,y) = x^2\\mathbf{i} + y^2\\mathbf{j}",
+  "Quadratic Field B":"\\mathbf{F}(x,y) = y^2\\mathbf{i} + x^2\\mathbf{j}",
+  "Cubic Field A":"\\mathbf{F}(x,y) = x^3\\mathbf{i} + y^3\\mathbf{j}",
+  "Cubic Field B":"\\mathbf{F}(x,y) = y^3\\mathbf{i} + x^3\\mathbf{j}",
+  "Improper Nodal Source":"\\mathbf{F}(x,y) = (15x-y)\\mathbf{i} + (16x+7y)\\mathbf{j}",
+  "Improper Nodal Sink":"\\mathbf{F}(x,y) = (-2x+y)\\mathbf{i} + (-4x+6y)\\mathbf{j}",
+  "Spiral Source":"\\mathbf{F}(x,y) = (-x+4y)\\mathbf{i} + (2x+3y)\\mathbf{j}",
+  "Stable Center":"\\mathbf{F}(x,y) = (x-2y)\\mathbf{i} + (x-y)\\mathbf{j}",
+  "Spiral Sink":"\\mathbf{F}(x,y) = (-x - 4y) \\mathbf{i} + (2x - 3y) \\mathbf{j}"
+};
 
 function setup() {
-  const totalHeight = window.innerHeight;
-  const totalWidth = window.innerWidth;
-
-  const navBarHeight = document.getElementById('navBar').clientHeight;
-  const detailBarHeight = document.getElementById('detailBar').clientHeight;
-
-  canvasHeight = totalHeight-navBarHeight-detailBarHeight;
-  canvasWidth = totalWidth;
+  adjustCanvasSize()
 
   hue_change_val = 1;
 
@@ -66,12 +85,10 @@ function setup() {
   field_name_elt = document.getElementById('field_name');
   field_name_elt.style.color = '#65C6FF';
 
-  define_flowfield()
+  vecChoice(current_field);
 }
 
 function draw() {
-
-  console.log(mouseX,mouseY);
 
   background(0,0,0,10);
   smooth();
@@ -86,12 +103,40 @@ function draw() {
     particles[i].aging();
     particles[i].checkdeath();
   }
+  if (show_axes){
+    push();
+    colorMode(RGB)
+    stroke(255,255,255,10);
+    strokeWeight(1);
+    line(-width/2,0,width/2,0);
+    line(0,height/2,0,-height/2);
+    pop();
+  }
 }
 
+// Sets the canvas size based on the window size
+function adjustCanvasSize() {
+  const totalHeight = window.innerHeight;
+  const totalWidth = window.innerWidth;
+
+  const navBarHeight = document.getElementById('navBar').clientHeight;
+  const detailBarHeight = document.getElementById('detailBar').clientHeight;
+
+  canvasHeight = totalHeight - navBarHeight - detailBarHeight;
+  canvasWidth = totalWidth;
+}
+
+// Adjusts the size of the canvas when window is resized
+function windowResized() {
+  setup();
+}
+
+// Clears the background
 function clearBG() {
   background(0);
 }
 
+// Resets all particles
 function resetParticles() {
   clearBG();
   for (let i = 0; i < particles.length; i++) {
@@ -99,32 +144,41 @@ function resetParticles() {
   }
 }
 
+// Toggles the visibility of the flowfield
 function show_ff() {
-  if (toggle_flow){
-    toggle_flow = false;
-  } else {
-    toggle_flow = true;
-  }
+  toggle_flow = !toggle_flow;
 }
 
 function vecChoice(field_name){
   resetParticles();
   current_field = field_name;
-  define_flowfield()
+  define_flowfield(current_field);
+  updateFormula(current_field);
 
   // Visually distinguish the selected button
-  let buttons = document.getElementById('vectorButtons').getElementsByTagName('button');
-  for (let i = 0; i < buttons.length; i++) {
-      if (buttons[i].innerHTML === field_name) {
-          buttons[i].classList.add('btn-selected'); // Add 'btn-selected' class to selected button
-      } else {
-          buttons[i].classList.remove('btn-selected'); // Remove 'btn-selected' class from other buttons
-      }
-  }
-
+  // let buttons = document.getElementById('vectorButtons').getElementsByTagName('button');
+  // for (let i = 0; i < buttons.length; i++) {
+  //     if (buttons[i].innerHTML === field_name) {
+  //         buttons[i].classList.add('btn-selected'); // Add 'btn-selected' class to selected button
+  //     } else {
+  //         buttons[i].classList.remove('btn-selected'); // Remove 'btn-selected' class from other buttons
+  //     }
+  // }
 }
 
-function define_flowfield(){
+//
+function updateFormula(current_field){
+  let formula = vecFormulas[current_field];
+  let formulaElement = document.getElementById("vector_field_formula");
+  
+  let math = MathJax.tex2chtml(formula);
+  formulaElement.innerHTML = "";
+  formulaElement.appendChild(math);
+  MathJax.startup.document.clear();
+  MathJax.startup.document.updateDocument();
+}
+
+function define_flowfield(current_field){
   let yoff = -canvasHeight/2; 
   for (let y = 0; y < rows; y++) {
     let xoff = -canvasWidth/2;
