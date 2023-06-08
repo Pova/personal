f646ca141df3b60ceba702f514ad5f87baf60868
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-08 18:21:26 +0100

    Update sketch.js

--- double_pendulum/sketch.js
+++ double_pendulum/sketch.js
@@ -1,6 +1,11 @@
 // Constants and configurations
 
-// ...
+let theta_1 = Math.PI;
+let theta_2 = Math.PI/8;
+const time_increment = 0.1 // Experiment with this
+const dampening = 0 ; // Need to experiment with this
+const pendulums =  [];
+let points = []
 
 // Page elements
 
@@ -13,14 +18,49 @@ function setup() {
     const canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('canvasContainer');
 
-    console.log(l_1,l_2,w_1,w_2,g);
+    angleMode(RADIANS)
+    colorMode(RGB)
 
+    if (pendulums.length == 0){
+        double_pendulum = new doublePendulum(
+            l_1,
+            l_2,
+            m_1,
+            m_2,
+            theta_1,
+            theta_2
+            )
+    
+        pendulums.push(double_pendulum);
+    }
   }
 
 
 function draw() {
+    background(0);
+    translate(width/2, height/3);
+    push()
+    strokeWeight(5);
+    point(0,0);
+    pop()
 
-    // Code here
+    // Loop through each object in the array
+    for (let i = 0; i < pendulums.length; i++) {
+        const object = pendulums[i];
+        object.update();
+        object.draw();
+  }
+
+    push();
+    beginShape();
+    for (let pt of points) {
+        noFill();
+        stroke(200);
+        strokeWeight(1);
+        vertex(pt[0], pt[1]);
+    }
+    endShape();
+    pop();
 
 }
 
@@ -38,13 +78,13 @@ function adjustCanvasSize() {
     canvasWidth = totalWidth;
   }
   
-// Adjusts the size of the canvas when window is resized
+//Adjusts the size of the canvas when window is resized
 function windowResized() {
     setup();
   }
 
 function clearBG() {
-  background(0);
+  points = [];
 }
 
 
