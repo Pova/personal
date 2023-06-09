96c51a1e9289ab2d82cc98618d65355319ffdd1c
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-10 00:00:24 +0100

    Add colours, reset button

--- double_pendulum/sketch.js
+++ double_pendulum/sketch.js
@@ -1,11 +1,17 @@
 // Constants and configurations
 
-let theta_1 = Math.PI;
-let theta_2 = Math.PI/8;
+let theta_1 = Math.random()*2*Math.PI;
+let theta_2 = Math.random()*2*Math.PI;
 const time_increment = 0.1 // Experiment with this
-const dampening = 0 ; // Need to experiment with this
-const pendulums =  [];
+const dampening = 0.0005; // Need to experiment with this value
+
+const pendulums =  []; // Currently just one
 let points = []
+
+let isDragging = false;
+let initialMousePos;
+let initial_theta_1;
+let initial_theta_2;
 
 // Page elements
 
@@ -52,19 +58,19 @@ function draw() {
   }
 
     push();
-    beginShape();
-    for (let pt of points) {
-        noFill();
-        stroke(200);
-        strokeWeight(1);
-        vertex(pt[0], pt[1]);
+    colorMode(HSB);
+    hue = 0;
+    for (let i=0;i<points.length-1;i++){
+      noFill();
+      strokeWeight(1);
+      stroke(hue,255,255,);
+      line(points[i][0],points[i][1],points[i+1][0],points[i+1][1]);
+      hue = (hue < 255) ? hue + 0.2 : 0;
     }
-    endShape();
+    smooth();
     pop();
 
 }
-
-
 
 // Sets the canvas size based on the window size
 function adjustCanvasSize() {
@@ -87,4 +93,39 @@ function clearBG() {
   points = [];
 }
 
+function resetAnimation(){
+  for (let i=0;i<pendulums.length;i++){
+    pendulums[i].update_theta_1(Math.random()*2*Math.PI)
+    pendulums[i].update_theta_2(Math.random()*2*Math.PI)
+  }
+  clearBG()
+}
+
+function euclideanDistance(point1, point2) {
+  var dx = point1.x - point2.x;
+  var dy = point1.y - point2.y;
+
+  return Math.sqrt(dx*dx + dy*dy);
+}
+
+// function mouseDragged() {
+//   const mid_pt = pendulums[0].calc_mid();
+//   const end_pt = [mid_pt[0] + pendulums[0].l_2 * sin(pendulums[0].theta_2), mid_pt[1] + pendulums[0].l_2 * cos(pendulums[0].theta_2)];
+
+//   if (pendulums[0].checkMouseDistance(createVector(mid_pt[0], mid_pt[1])) || pendulums[0].checkMouseDistance(createVector(end_pt[0], end_pt[1]))) {
+//     isDragging = true;
+//     initialMousePos = createVector(mouseX - width / 2, mouseY - height / 3);
+//     initialMouseTheta = atan2(initialMousePos.y,initialMousePos.x)
+//     console.log(initialMousePos,initialMouseTheta)
+//     initial_theta_1 = pendulums[0].theta_1;
+//     initial_theta_2 = pendulums[0].theta_2;
+//     return false;
+//   }
+// }
+
+// function mouseReleased() {
+//   if (isDragging) {
+//     isDragging = false;
+//   }
+// }
 
