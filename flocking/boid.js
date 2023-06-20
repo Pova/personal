ac7137c74ab87bacfa83abe35c1ec032af0a07bc
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-20 16:02:28 +0100

    Implement cohesion and separation

--- flocking/boid.js
+++ flocking/boid.js
@@ -1,14 +1,30 @@
 class Boid {
     constructor() {
-        this.position = createVector(random(0,width), random(0,height));
+        this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
-        this.velocity.setMag(random(0.5,1.5));
+        this.velocity.setMag(random(2,4));
         this.acceleration = createVector();
-        this.maxForce = 0.01;
+        this.maxForce = .2;
+        this.maxSpeed = 3;
     }
 
-    align(boids){
-        let steering = createVector();
+    edges(){
+        if (this.position.x>width){
+            this.position.x = 0;
+        } else if (this.position.x<0){
+            this.position.x = width;
+        }
+        if (this.position.y>height){
+            this.position.y = 0;
+        } else if (this.position.y<0){
+            this.position.y = height;
+        }
+    }
+
+    behaviours(boids){
+        let align_steering = createVector();
+        let cohesion_steering = createVector();
+        let separation_steering = createVector();
         let total = 0;
         for (let other of boids){
             const d = dist(
@@ -18,28 +34,59 @@ class Boid {
                 other.position.y
             );
             if (other != this && d<perception){
-                steering.add(other.velocity);
+                // Alignment 
+                align_steering.add(other.velocity);
+                // Cohesion
+                cohesion_steering.add(other.position);
+                // Separation
+                const diff = p5.Vector.sub(this.position,other.position);
+                diff.div(d);
+                separation_steering.add(diff);
+
                 total++;
             }
         }
         if (total>0){
-            steering.div(total);
-            steering.sub(this.velocity);
-            steering.limit(this.maxForce);
+            // Alignment behaviours
+            align_steering.div(total);
+            align_steering.setMag(this.maxSpeed);
+            align_steering.sub(this.velocity);
+            align_steering.limit(this.maxForce);
+            
+            align_steering.mult(boid_alignment/50);
+            
+            // Cohesion behaviours
+            cohesion_steering.div(total);
+            cohesion_steering.sub(this.position);
+            cohesion_steering.setMag(this.maxSpeed);
+            cohesion_steering.sub(this.velocity);
+            cohesion_steering.limit(this.maxForce);
+            
+            cohesion_steering.mult(boid_cohesion/50);
+            
+            // Separation behaviours
+            separation_steering.div(total);
+            separation_steering.setMag(this.maxSpeed);
+            separation_steering.sub(this.velocity);
+            separation_steering.limit(this.maxForce/50);
+            
+            separation_steering.mult(boid_separation);
         }
-        return steering;
-        
+        return [align_steering,cohesion_steering,separation_steering]; 
     }
 
     flock_calculation(flock){
-        const alignment = this.align(flock);
-        this.acceleration = alignment;
+        const behaviours = this.behaviours(flock);
+        for (let force of behaviours){
+            this.acceleration.add(force);
+        }
     }
 
     update(){
         this.velocity.add(this.acceleration);
         this.position.add(this.velocity);
-        this.acceleration.setMag(0);   
+        this.velocity.limit(this.maxSpeed)
+        this.acceleration.mult(0);   
     }
 
     show(){
