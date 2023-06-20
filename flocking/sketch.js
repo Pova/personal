d74b939225cc92326dfd6d5bfd9ecc166499a627
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-20 09:49:04 +0100

    Create initial sketch

---
+++ flocking/sketch.js
@@ -0,0 +1,42 @@
+const boid_length = 10; //length in pixels
+const boid_width = 6; //width in pixels
+
+const perception = 50;
+
+const flock = [];
+const flock_size = 100;
+
+function setup() {
+    adjustCanvasSize()
+    
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent('canvasContainer');
+
+    // angleMode(RADIANS);
+
+    for (let i=0;i<flock_size; i++){
+        flock.push(new Boid());
+    }
+  }
+
+  function draw(){
+    background(51);
+
+    for (let boid of flock){
+        boid.flock_calculation(flock);
+        boid.update();
+        boid.show();
+    }
+  }
+
+// Sets the canvas size based on the window size
+function adjustCanvasSize() {
+    const totalHeight = window.innerHeight;
+    const totalWidth = window.innerWidth;
+  
+    const navBarHeight = document.getElementById('navBar').clientHeight;
+    const detailBarHeight = document.getElementById('detailBar').clientHeight;
+  
+    canvasHeight = totalHeight - navBarHeight - detailBarHeight;
+    canvasWidth = totalWidth;
+  }
