b93b7dc4872daf658ea46252407b1afc56f239f7
Roman Kitsela <roman.kitsela@gmail.com>
2023-06-30 23:59:09 +0100

    Add food

--- evolutionary_steering/sketch.js
+++ evolutionary_steering/sketch.js
@@ -5,15 +5,24 @@ const perception = 50;
 
 const vehicles = [];
 
+const food_amount = 10;
+const poison_amount = 10;
+const food = [];
+const poison = [];
+
 
 function setup() {
     adjustCanvasSize();
-    
     const canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('canvasContainer');
 
     vehicles.push(new Vehicle())
 
+    for (let i=0;i<food_amount;i++){
+      const x = random(width);
+      const y = random(height);
+      food.push(createVector(x,y));
+    }
   }
 
   function draw(){
@@ -21,8 +30,17 @@ function setup() {
 
     const target = createVector(mouseX,mouseY);
 
+    for (let i = 0; i<food.length; i++){
+      push();
+      fill(0,255,0);
+      noStroke();
+      ellipse(food[i].x,food[i].y,6,6);
+      pop();
+    }
+
     for (let vehicle of vehicles){
-        vehicle.seek(target);
+        //vehicle.seek(target);
+        vehicle.eat(food);
         vehicle.update();
         vehicle.edges()
         vehicle.show();
